7ea5b4764d5fca74cfa987c3df6c659a2e85c413
Sxtanna <ranald.taylor@gmail.com>
2021-12-14 17:25:20 -0500

    feat: added mob entity tracking methods

--- src/main/java/com/sxtanna/mc/mb/MobBorderPlugin.java
+++ src/main/java/com/sxtanna/mc/mb/MobBorderPlugin.java
@@ -1,13 +1,20 @@
 package com.sxtanna.mc.mb;
 
 import com.sxtanna.mc.mb.conf.Config;
+import com.sxtanna.mc.mb.conf.sections.BorderSettings;
+import com.sxtanna.mc.mb.conf.sections.EntitySettings;
 import com.sxtanna.mc.mb.data.MobBorderEntity;
+import com.sxtanna.mc.mb.util.LocationCodec;
 import io.papermc.paper.event.entity.EntityMoveEvent;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import org.bukkit.NamespacedKey;
+import org.bukkit.entity.Entity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
+import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
@@ -15,6 +22,7 @@ import org.jetbrains.annotations.Nullable;
 
 import java.util.Objects;
 import java.util.Optional;
+import java.util.UUID;
 
 public final class MobBorderPlugin extends JavaPlugin implements Listener {
 
@@ -45,11 +53,17 @@ public final class MobBorderPlugin extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
         this.getServer().getPluginManager().registerEvents(this, this);
+
+        findMobBorderEntity();
+        loadMobBorderEntity();
+
     }
 
     @Override
     public void onDisable() {
         HandlerList.unregisterAll(((Listener) this));
+
+        killMobBorderEntity();
 
         INSTANCE = null;
     }
@@ -80,6 +94,66 @@ public final class MobBorderPlugin extends JavaPlugin implements Listener {
     @Contract(pure = true)
     public @NotNull Optional<MobBorderEntity> getEntity() {
         return Optional.ofNullable(this.entity);
+    }
+
+
+    public void findMobBorderEntity() {
+        final var uuid = getConfiguration().get(EntitySettings.ENTITY_UUID);
+        if (uuid.isBlank()) {
+            return;
+        }
+
+        try {
+            this.entity = new MobBorderEntity(UUID.fromString(uuid));
+        } catch (final IllegalArgumentException ignored) {
+            // nothing
+        }
+    }
+
+    public void loadMobBorderEntity() {
+        killMobBorderEntity();
+
+        final var origin = LocationCodec.decode(getConfiguration().get(BorderSettings.BORDER_ORIGIN))
+                                        .orElse(null);
+
+        if (origin == null) {
+            return;
+        }
+
+        final var entity = origin.getWorld().spawnEntity(origin,
+                                                         getConfiguration().get(EntitySettings.ENTITY_TYPE));
+
+        entity.getPersistentDataContainer()
+              .set(namespace, PersistentDataType.PrimitivePersistentDataType.BYTE, ((byte) 1));
+
+
+        final var name = getConfiguration().get(EntitySettings.ENTITY_NAME);
+
+        if (name.isBlank()) {
+            entity.setCustomNameVisible(false);
+            entity.customName(Component.empty());
+        } else {
+            entity.setCustomNameVisible(true);
+            entity.customName(LegacyComponentSerializer.legacyAmpersand().deserialize(name));
+        }
+
+        entity.setInvulnerable(true);
+        entity.setSilent(getConfiguration().get(EntitySettings.ENTITY_SILENT));
+
+        this.entity = new MobBorderEntity(entity.getUniqueId());
+
+        getConfiguration().setProperty(EntitySettings.ENTITY_UUID, entity.getUniqueId().toString());
+        getConfiguration().save();
+    }
+
+    public void killMobBorderEntity() {
+        getEntity().flatMap(MobBorderEntity::live)
+                   .ifPresent(Entity::remove);
+
+        this.entity = null;
+
+        getConfiguration().setProperty(EntitySettings.ENTITY_UUID, "");
+        getConfiguration().save();
     }
 
 }
