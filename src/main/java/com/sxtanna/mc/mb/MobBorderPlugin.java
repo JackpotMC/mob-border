1d20f3d15cc9083256eb0d69e4254d39f63e5ab8
Sxtanna <ranald.taylor@gmail.com>
2021-12-14 16:17:39 -0500

    chore: initial commit

---
+++ src/main/java/com/sxtanna/mc/mb/MobBorderPlugin.java
@@ -0,0 +1,23 @@
+package com.sxtanna.mc.mb;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MobBorderPlugin extends JavaPlugin {
+
+
+    @Override
+    public void onLoad() {
+
+    }
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+}
