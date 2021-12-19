6e1dab0c77995c1c7bffeb0f54eee58092dda57a
Sxtanna <ranald.taylor@gmail.com>
2021-12-19 01:57:39 -0500

    feat: added parameter completion for commands

--- src/main/java/com/sxtanna/mc/mb/cmds/MobBorderCommand.java
+++ src/main/java/com/sxtanna/mc/mb/cmds/MobBorderCommand.java
@@ -3,6 +3,7 @@ package com.sxtanna.mc.mb.cmds;
 import co.aikar.commands.BaseCommand;
 import co.aikar.commands.CommandHelp;
 import co.aikar.commands.annotation.CommandAlias;
+import co.aikar.commands.annotation.CommandCompletion;
 import co.aikar.commands.annotation.CommandPermission;
 import co.aikar.commands.annotation.Default;
 import co.aikar.commands.annotation.Flags;
@@ -67,6 +68,7 @@ public final class MobBorderCommand extends BaseCommand {
 
     @Subcommand("resize")
     @CommandPermission("jmc.mobborder.resize")
+    @CommandCompletion("250")
     public void resize(@NotNull final CommandSender sender, @Default("250") @Flags("min=16,max=10000") final int size) {
         this.plugin.getConfiguration().setProperty(BorderSettings.BORDER_SIZE, ((double) size));
         this.plugin.getConfiguration().save();
@@ -78,6 +80,7 @@ public final class MobBorderCommand extends BaseCommand {
 
     @Subcommand("damage")
     @CommandPermission("jmc.mobborder.damage")
+    @CommandCompletion("0.2")
     public void damage(@NotNull final CommandSender sender, @Default("0.2") @Flags("min=0.0,max=20.0") final double damage) {
         this.plugin.getConfiguration().setProperty(BorderSettings.BORDER_HURT, damage);
         this.plugin.getConfiguration().save();
@@ -89,6 +92,7 @@ public final class MobBorderCommand extends BaseCommand {
 
     @Subcommand("scaling")
     @CommandPermission("jmc.mobborder.scaling")
+    @CommandCompletion("true|false")
     public void scaling(@NotNull final CommandSender sender, @Default("false") final boolean scaling) {
         this.plugin.getConfiguration().setProperty(BorderSettings.BORDER_SCALING, scaling);
         this.plugin.getConfiguration().save();
@@ -112,6 +116,7 @@ public final class MobBorderCommand extends BaseCommand {
 
     @Subcommand("entity silent")
     @CommandPermission("jmc.mobborder.entity.silent")
+    @CommandCompletion("true|false")
     public void silent(@NotNull final CommandSender sender, @Default("false") final boolean silent) {
         this.plugin.getConfiguration().setProperty(EntitySettings.ENTITY_SILENT, silent);
         this.plugin.getConfiguration().save();
@@ -123,6 +128,7 @@ public final class MobBorderCommand extends BaseCommand {
 
     @Subcommand("entity glowing")
     @CommandPermission("jmc.mobborder.entity.glowing")
+    @CommandCompletion("true|false")
     public void glowing(@NotNull final CommandSender sender, @Default("true") final boolean glowing) {
         this.plugin.getConfiguration().setProperty(EntitySettings.ENTITY_GLOWING, glowing);
         this.plugin.getConfiguration().save();
@@ -134,6 +140,7 @@ public final class MobBorderCommand extends BaseCommand {
 
     @Subcommand("entity speed")
     @CommandPermission("jmc.mobborder.entity.speed")
+    @CommandCompletion("0.25")
     public void speed(@NotNull final CommandSender sender, @Default("0.25") @Flags("min=0.0,max=1024.0") final double speed) {
         this.plugin.getConfiguration().setProperty(EntitySettings.ENTITY_SPEED, speed);
         this.plugin.getConfiguration().save();
@@ -145,6 +152,7 @@ public final class MobBorderCommand extends BaseCommand {
 
     @Subcommand("entity freeze")
     @CommandPermission("jmc.mobborder.entity.freeze")
+    @CommandCompletion("true|false")
     public void freeze(@NotNull final CommandSender sender, @Default("false") final boolean frozen) {
         this.plugin.getConfiguration().setProperty(EntitySettings.ENTITY_FROZEN, frozen);
         this.plugin.getConfiguration().save();
@@ -157,6 +165,7 @@ public final class MobBorderCommand extends BaseCommand {
 
     @Subcommand("entity respawns")
     @CommandPermission("jmc.mobborder.entity.respawns")
+    @CommandCompletion("true|false")
     public void respawns(@NotNull final CommandSender sender, @Default("false") final boolean respawns) {
         this.plugin.getConfiguration().setProperty(EntitySettings.ENTITY_RESPAWNS, respawns);
         this.plugin.getConfiguration().save();
